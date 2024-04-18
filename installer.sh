1a7f1f74fae93e53f9e6c1eda43d763b6bba0162
1ly4s0 <67460389+1ly4s0@users.noreply.github.com>
2024-04-18 15:57:39 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -2,7 +2,6 @@
 
 sudo apt update
 
-sudo nano /etc/apt/sources.list
 echo "deb http://download.webmin.com/download/repository sarge contrib" | sudo tee -a /etc/apt/sources.list
 
 wget -q -O- http://www.webmin.com/jcameron-key.asc | sudo apt-key add -
